ab8cf09b73a0af7930b8908713bcf799ecd19e0b
Cameron Rison <cameron.rison@utexas.edu>
2019-04-02 18:11:57 -0500

    Forgot these

---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PrimaryNav from "./PrimaryNav";
+import Footer from "./Footer";
+
+export const Product = () => {
+  return (
+    <div>
+      <PrimaryNav />
+      <h1>Blog</h1>
+      <Footer />
+    </div>
+  );
+};
