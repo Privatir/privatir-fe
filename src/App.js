b8e07d323aa9165d844d9df879b0d1e761f1749e
Cameron Rison <cameron.rison@utexas.edu>
2019-04-02 18:08:39 -0500

    Reducing routes for no

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import { About } from './components/About'
-import { Contact } from './components/Contact'
-import { Blog } from './components/Blog'
-import Landing from './components/Landing';
-import '@material/react-button/dist/button.css';
-import './App.css';
-import '@material/react-layout-grid/dist/layout-grid.css';
+import React from "react";
+import { Product } from "./components/Product";
+import Landing from "./components/Landing";
+import "@material/react-button/dist/button.css";
+import "./App.scss";
+import "@material/react-layout-grid/dist/layout-grid.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import typography from './utils/typography'
+import typography from "./utils/typography";
 
-import { Grid } from '@material/react-layout-grid';
+import { Grid } from "@material/react-layout-grid";
 
 class App extends React.Component {
   render() {
     return (
-      < Router >
+      <Router>
         <Grid>
           {typography.injectStyles()}
           <Route path="/" exact component={Landing} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/blog" exact component={Blog} />
+          <Route path="/product" exact component={Product} />
         </Grid>
-      </Router >
+      </Router>
     );
   }
 }
 
-export default App
+export default App;
--- src/components/PrimaryNav.jsx
+++ src/components/PrimaryNav.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
-import Button from '@material/react-button';
-import styled from 'styled-components'
+import React from "react";
+import Button from "@material/react-button";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Cell, Row } from '@material/react-layout-grid';
+import { Cell, Row } from "@material/react-layout-grid";
 
 const StyledAnchor = styled(Link)`
-    text-transform: uppercase;
-    color: #4D2C91;
-    text-decoration: none;
-    &:hover {
-      border-bottom: 4px solid currentColor;
-      display: inline-block;
-    }
-    `
+  color: #4d2c91;
+  font-weight: bold;
+  text-decoration: none;
+  &:hover {
+    border-bottom: 4px solid currentColor;
+    display: inline-block;
+  }
+`;
 
 const PrimaryNav = () => {
-    return (
-        < Row >
-            <Cell columns={8}>
-                <svg width="150" height="100"
-                    xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                    <a href="/">
-                        <image xlinkHref="https://s3.amazonaws.com/privatir.com/Privatir-logo.svg" height="100" width="150" />
-                    </a>
-                </svg>
-            </Cell>
-            <Cell columns={4} style={{ justifyContent: `space-around`, display: `flex`, alignItems: `center` }}>
-                <StyledAnchor to="/about">About</StyledAnchor>
-                <StyledAnchor to="/contact">Contact</StyledAnchor>
-                <StyledAnchor to="/blog">Blog</StyledAnchor>
-                <Button outlined={true}>Sign In</Button>
-            </Cell>
-        </Row >
-    )
-}
+  return (
+    <Row>
+      <Cell columns={8}>
+        <svg
+          width="150"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <a href="/">
+            <image
+              xlinkHref="https://s3.amazonaws.com/privatir.com/Privatir-logo.svg"
+              height="100"
+              width="150"
+            />
+          </a>
+        </svg>
+      </Cell>
+      <Cell
+        columns={4}
+        style={{
+          justifyContent: `space-evenly`,
+          display: `flex`,
+          alignItems: `center`
+        }}
+      >
+        <StyledAnchor to="/product">Product</StyledAnchor>
+        <Button href="https://app.privatir.com/signin" outlined={true}>
+          Sign In
+        </Button>
+      </Cell>
+    </Row>
+  );
+};
 
-export default PrimaryNav
+export default PrimaryNav;
