103e4d6a03d8d2c3f22b96bbf08a3c0a29774251
killakam3084 <cameron.rison@utexas.edu>
2019-04-29 18:53:51 -0500

    Adding back output build dir. We deploy pre-built.

---
+++ build/precache-manifest.c7d18e36874088b9dad77b8bee63a4bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "60119ab8ef415c19c0e9c0efe2de14ce",
+    "url": "/static/media/cam-avatar.60119ab8.jpg"
+  },
+  {
+    "revision": "680153ffe843da9244f3",
+    "url": "/static/css/main.24e3b2c1.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d43ae19266a81b89dbfd",
+    "url": "/static/js/2.e5436413.chunk.js"
+  },
+  {
+    "revision": "07321148db8e1aaf2d01b26eed4b14c1",
+    "url": "/static/media/market-size.07321148.png"
+  },
+  {
+    "revision": "859204311fe5bf62d09446114c0ecd30",
+    "url": "/static/media/product-roadmap.85920431.png"
+  },
+  {
+    "revision": "680153ffe843da9244f3",
+    "url": "/static/js/main.9ff149c7.chunk.js"
+  },
+  {
+    "revision": "72e01861d5b3888c10af2e0ea56a6e7d",
+    "url": "/static/media/james-avatar.72e01861.jpg"
+  },
+  {
+    "revision": "b6f5664266bdf8c1078b8e19fa6a6b27",
+    "url": "/static/media/michael-avatar.b6f56642.jpg"
+  },
+  {
+    "revision": "ec4ecbf2d74fb4c472b51b8eb25a56a9",
+    "url": "/static/media/lifepreserver.ec4ecbf2.png"
+  },
+  {
+    "revision": "a95d8440c952274bf41fbb63a14be599",
+    "url": "/static/media/waves.a95d8440.png"
+  },
+  {
+    "revision": "8aedf1387fb713f0dcc47b129cfa8028",
+    "url": "/index.html"
+  }
+];
